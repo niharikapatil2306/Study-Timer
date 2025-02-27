5293e409d924c13419e40606e09d0776835385c7
niharikapatil2306 <patilniharika2306@gmail.com>
2025-02-27 23:50:21 +0000

    Ignore .history folder

--- .history/clock_20250215002904.js
+++
@@ -1,3 +0,0 @@
-let timer;
-let timeLeft = 50*60 // 50 minutes
-const min = document.getElementById("min");
--- .history/clock_20250215011956.js
+++
@@ -1,5 +0,0 @@
-let timer;
-let timeLeft = 50*60 // 50 minutes
-const min = document.getElementById("min");
-const sec = document.getElementById("sec");
-const startBtn = document.getElementById("start");
